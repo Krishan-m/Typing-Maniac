e18beb7ccb48dbe5656a5fd1a26a66190f60e3e7
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-04-04 16:36:24 -0400

    Add correct color to letters

--- resources/typer.js
+++ resources/typer.js
@@ -14,17 +14,17 @@ function startTyping() {
     // typeHere.insertAdjacentElement('beforeend', letterHighlighter);
     // typeHere.insertAdjacentElement("beforeend", toBeTypedText);
     // typeHere.addEventListener("keydown", (event)=>{
-        //     let keyPressed = event.key; 
-        //     console.log(keyPressed);
-        //     console.log(currentLetter)
-        //     if (keyPressed == currentLetter){
-            //         correctLetters++;
-            //         letterHighlighter.classList.remove('highlight-incorrect');
-            //         letterHighlighter.classList.add('highlight-correct');
-            //     }
-            //     else{
-                //         wrongLetters++;
-                //         letterHighlighter.classList.remove('highlight-correct');
+    //     let keyPressed = event.key; 
+    //     console.log(keyPressed);
+    //     console.log(currentLetter)
+    //     if (keyPressed == currentLetter){
+    //         correctLetters++;
+    //         letterHighlighter.classList.remove('highlight-incorrect');
+    //         letterHighlighter.classList.add('highlight-correct');
+    //     }
+    //     else{
+    //         wrongLetters++;
+    //         letterHighlighter.classList.remove('highlight-correct');
     //         letterHighlighter.classList.add('highlight-incorrect');
     //     }
     //     letterHighlighter.innerHTML = currentLetter;
@@ -32,27 +32,36 @@ function startTyping() {
     //     typedText.innerHTML = typingText.slice(0, currentLetterIndex-1);
     //     toBeTypedText.innerHTML = typingText.slice(currentLetterIndex);
     //     if (currentLetterIndex == totalLetters){
-        //         console.log("Done");
-        //         console.log(correctLetters);
-        //         console.log(wrongLetters);
-        //     }
-        // })
-        let letters = []; // Letters in the paragraph
-        for (let word of typeHere.children){
-            for (let letter of word.children){
-                letters.push(letter);
-            }
+    //         console.log("Done");
+    //         console.log(correctLetters);
+    //         console.log(wrongLetters);
+    //     }
+    // })
+    let letters = []; // Letters in the paragraph
+    for (let word of typeHere.children) {
+        for (let letter of word.children) {
+            letters.push(letter);
         }
-        // Use .innerHTML to access the current letter
-        let currentLetterIndex = 0;
-        let currentLetter = letters[currentLetterIndex];
-        let correctLetters = 0;
-        let incorrectLetters = 0;
-        let totalLetters = letters.length;
-        typeHere.addEventListener("keydown", (event) => {
-            let keyPressed = event.key;
-            console.log(keyPressed);
+    }
+    // Use .innerHTML to access the current letter
+    let currentLetterIndex = 0;
+    let currentLetter = letters[currentLetterIndex];
+    let correctLetters = 0;
+    let incorrectLetters = 0;
+    let totalLetters = letters.length;
+    restartTyping(letters, currentLetterIndex, currentLetter, correctLetters, incorrectLetters);
+    typeHere.addEventListener("keydown", (event) => {
+        console.log(event)
+        let keyPressed = event.key;
+        let keyCode = keyPressed.length == 1 ? keyPressed.charCodeAt(0) : 0;
+        console.log(keyCode);
+        if ((keyCode >= 65 && keyCode <= 90)
+            || (keyCode >= 97 && keyCode <= 122)
+            // || (keyCode >= 48 && keyCode <= 57) // For numbers
+            || keyCode == 32) { // 32 for space 
+                console.log(keyPressed)
             console.log(currentLetter.innerHTML)
+
             if (keyPressed == currentLetter.innerHTML) {
                 correctLetters++;
                 currentLetter.classList.add('highlight-correct');
@@ -68,7 +77,8 @@ function startTyping() {
                 console.log(wrongLetters);
                 return;
             }
-        })
+        }
+    })
 }
 
 function createPara(sampleText) {
@@ -98,6 +108,19 @@ function createPara(sampleText) {
             wordSpan = document.createElement('span');
         }
     }
+}
+
+function restartTyping(...data) {
+    console.log(data)
+    let letters = data[0]
+    data[1] = 0;
+    data[2] = letters[data[1]];
+    data[3] = 0;
+    data[4] = 0;
+    letters.forEach(letter => {
+        letter.classList.remove('highlight-correct');
+        letter.classList.remove('highlight-incorrect');
+    });
 }
 
 const typeHere = document.getElementById('typeHere');
